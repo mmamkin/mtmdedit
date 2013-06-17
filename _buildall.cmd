79f622cd3113919782da10bc1878de10f80eb7cf
Mansur Mamkin <devnull@localhost>
2013-06-17 12:10:12 +0600

    мелкие исправления

--- _buildall.cmd
+++ _buildall.cmd
@@ -1,1 +1,1 @@
-msbuild /p:Configuration="arx2012;arx2013" /p:Platform="Win32;x64" /p:Projects="MtmdEdit.vcxproj" _MtmdBuild.proj
+msbuild /p:Configuration="arx2012;arx2013;brx13" /p:Platform="Win32;x64" /p:Projects="MtmdEdit.vcxproj" _MtmdBuild.proj
