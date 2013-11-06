c877cad3760198bfed54846203c996eb0680f353
Mansur Mamkin <devnull@localhost>
2013-11-06 12:55:27 +0600

    add BRX14 support

--- _buildall.cmd
+++ _buildall.cmd
@@ -1,4 +1,4 @@
 set MTMDPROJECTS=MtmdEdit.vcxproj
 
 call "%VS100COMNTOOLS%vsvars32.bat"
-msbuild /p:Configuration="arx2012;arx2013;brx13" /p:Platform="Win32;x64" /p:Projects="%MTMDPROJECTS%" _MtmdBuild.proj
+msbuild /p:Configuration="arx2012;arx2013;brx13;brx14" /p:Platform="Win32;x64" /p:Projects="%MTMDPROJECTS%" _MtmdBuild.proj
