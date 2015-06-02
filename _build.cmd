5ce5f6a67b6720d619760c6da6433aaefee1f4f3
mmamkin <devnull@localhost>
2015-06-02 15:40:52 +0500

    Added ARX2015 support

--- _build.cmd
+++ _build.cmd
@@ -1,3 +1,3 @@
 call "%VS100COMNTOOLS%vsvars32.bat"
-msbuild /p:Configuration=arx2012 /p:Platform=x64 MtmdEdit.vcxproj
+msbuild /p:Configuration=arx2015 /p:Platform=x64 MtmdEdit.vcxproj
 pause
--- _buildall.cmd
+++ _buildall.cmd
@@ -1,5 +1,5 @@
 set MTMDPROJECTS=MtmdEdit.vcxproj
 
-call "%VS100COMNTOOLS%vsvars32.bat"
-rem msbuild /p:Configuration="arx2012;arx2013;brx13;brx14;brx15" /p:Platform="Win32;x64" /p:Projects="%MTMDPROJECTS%" _MtmdBuild.proj
-msbuild /p:Configuration="brx15" /p:Platform="Win32;x64" /p:Projects="%MTMDPROJECTS%" _MtmdBuild.proj
+rem call "%VS100COMNTOOLS%vsvars32.bat"
+call "%VS110COMNTOOLS%vsvars32.bat"
+msbuild /p:Configuration="arx2015" /p:Platform="Win32;x64" /p:Projects="%MTMDPROJECTS%" /p:VCTargetsPath="C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V110" _MtmdBuild.proj
